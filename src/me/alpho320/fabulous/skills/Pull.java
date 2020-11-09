ff75f8bd3911734331fbe5c3fbb6729e2a38326b
Alpho320 <cahitbaba00@gmail.com>
2020-11-10 02:47:07 +0300

    1.0.1 - bugs fixed

--- src/me/alpho320/fabulous/skills/Pull.java
+++ src/me/alpho320/fabulous/skills/Pull.java
@@ -1,33 +1,55 @@
 package me.alpho320.fabulous.skills;
 
-import me.alpho320.fabulous.guardian.Main;
-import me.alpho320.fabulous.guardian.extension.Skill;
+import me.alpho320.fabulous.bosses.Main;
+import me.alpho320.fabulous.bosses.extension.Skill;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pull extends Skill {
+
+    Map<LivingEntity, LocalDateTime> timeMap = new HashMap<>();
 
     public Pull(Main main) {
         super(main);
     }
 
     @Override
-    public void use(Entity entity, long tick) {
+    public void use(LivingEntity entity, long tick) {
         if(entity.isDead()) return;
+        if(!timeMap.containsKey(entity))
+            timeMap.put(entity, LocalDateTime.now().plusSeconds(tick/20));
 
-        Collection<Entity> entityList = entity.getLocation().getWorld().getNearbyEntities(
-                entity.getLocation(),
-                30,
-                15,
-                20
-        );
+        if(ChronoUnit.SECONDS.between(LocalDateTime.now(), timeMap.get(entity)) > 0) return;
 
-        entityList.forEach(e -> e.setVelocity(entity.getLocation().getDirection()));
+        for (Entity e : entity.getNearbyEntities(20, 20, 20)) {
+            if (e instanceof Player) {
+                Player p = (Player) e;
 
-        main.getServer().getScheduler().runTaskLater(main, () -> {
-            use(entity, tick);
-        }, tick);
+                Location playerCenterLocation = entity.getEyeLocation();
+                Location playerToThrowLocation = p.getEyeLocation();
+
+                double x = playerToThrowLocation.getX() - playerCenterLocation.getX();
+                double y = playerToThrowLocation.getY() - playerCenterLocation.getY();
+                double z = playerToThrowLocation.getZ() - playerCenterLocation.getZ();
+
+                Vector throwVector = new Vector(x, y, z);
+
+                throwVector.normalize();
+                throwVector.multiply(-1.0D);
+                throwVector.setY(0.1D);
+
+                p.setVelocity(throwVector);
+            }
+        }
+        timeMap.put(entity, LocalDateTime.now().plusSeconds(tick/20));
     }
 
     @Override
